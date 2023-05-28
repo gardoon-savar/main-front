0ccc0b1f38b2e9bfb0148a61a4420b6076be01d2
Shamim <mim24sd@gmail.com>
2023-05-28 19:38:54 +0330

    My changes -add css module -add sass -add variables -config package json -add description -add reset css

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+
+import "./index.css";
+
 import App from "./App.jsx";
 
 const root = createRoot(document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
@@ -19,8 +21,37 @@ module.exports = {
         },
       },
       {
-        test: /\.(scss)$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
       },
       {
         test: /\.(png|jp(e*)g|svg|gif)$/,
@@ -32,6 +63,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "index.html"),
     }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css",
+    }),
   ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss']
+  },
   mode: "development",
 };
