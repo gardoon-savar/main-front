90a3cf6de971f167668a348d9b901a2d252ca8d2
Shamim <mim24sd@gmail.com>
2023-05-29 19:58:01 +0330

    feature/add header component

---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles/index.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.main}>
+      <h1>گردون سوار!</h1>
+    </header>
+  );
+};
+
+export default Header;
