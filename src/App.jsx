c46c261b30834271076b9311902d167700491eb6
Shamim <mim24sd@gmail.com>
2023-09-23 17:17:02 +0330

    add vite to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import React from "react";
-
 import Header from "./components/header/main";
 import AboutMe from "./components/aboutMe";
 import FastSearch from "./components/fastSearch/main";
 
-const App = () => {
+function App() {
   return (
     <div>
       <Header />
-      <FastSearch />
-      <AboutMe />
+      {/* <FastSearch />
+      <AboutMe /> */}
     </div>
   );
-};
+}
 
 export default App;
--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-import "./index.module.scss";
-
-import App from "./App.jsx";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
